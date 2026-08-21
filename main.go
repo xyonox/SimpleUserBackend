b26ef3f64e72f504af931f3f268405c8aac3882c
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-21 14:45:55 +0200

    Simple basic backend (Hello world)

--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
+	"net/http"
+
+	_ "modernc.org/sqlite"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+const (
+	dbFile = "SimpleUserBackend.db"
+)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+// For database
+// go get modernc.org/sqlite
+
+func loadDB() (*sql.DB, error) {
+	return nil, nil
+}
+
+func httpTest() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello, world!"))
+		if err != nil {
+			return
+		}
+	}
+}
+
+func run() error {
+
+	/*db, err := loadDB()
+	if err != nil {
+		return err
+	}
+	if db == nil {
+		return errors.New("db is nil")
+	}
+	*/
+
+	http.HandleFunc("/helloworld", httpTest())
+
+	return http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
 	}
 }
