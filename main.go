fd8b9771be96263718a324ab65520b10906a4242
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-22 15:03:25 +0200

    get Users http request handler added

--- main.go
+++ main.go
@@ -23,6 +23,41 @@ type User struct {
 	PasswordHash string `json:"password_hash"`
 }
 
+func getUsers(db *sql.DB) ([]*User, error) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			fmt.Println("error closing rows")
+		}
+	}(rows)
+
+	users := []*User{}
+	for rows.Next() {
+		user := User{}
+		err := rows.Scan(&user.ID, &user.Name, &user.PasswordHash)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	return users, nil
+}
+
+func getUser(db *sql.DB, id int) (*User, error) {
+	row := db.QueryRow("SELECT * FROM users WHERE id = ?", id)
+	user := User{}
+	err := row.Scan(&user.ID, &user.Name, &user.PasswordHash)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func loadDB() (*sql.DB, error) {
 	db, err := sql.Open("sqlite", dbFile)
 	if err != nil {
@@ -42,6 +77,21 @@ func httpTest() http.HandlerFunc {
 		if err != nil {
 			return
 		}
+	}
+}
+
+func httpGetUsers(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := getUsers(db)
+		if err != nil {
+			_, err := w.Write([]byte(err.Error()))
+			if err != nil {
+				return
+			}
+			return
+		}
+
+		_, err = w.Write([]byte(fmt.Sprintf("%v", users)))
 	}
 }
 
@@ -65,6 +115,7 @@ func run() error {
 	fmt.Println("starting server")
 
 	http.HandleFunc("/helloworld", httpTest())
+	http.HandleFunc("/users", httpGetUsers(db))
 
 	fmt.Println("server started on port ", port)
 
