c49f207fbb98630947385504f35dadb82f6a5c2d
Xyonox <93388182+xyonox@users.noreply.github.com>
2026-08-23 15:02:04 +0200

    Build own simple password hashing algorithm - need to test and optimize

--- handlers/security_handler.go
+++ handlers/security_handler.go
@@ -1,9 +1,92 @@
 package handlers
 
-func HashPassword(password string) string {
-	return password
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+func HashPasswordWithSalt(password string, salt []byte) ([]byte, error) {
+
+	saltedPassword := append(salt, []byte(password)...)
+
+	var states []byte
+	for _, saltChar := range saltedPassword {
+		states = append(states, saltChar)
+	}
+
+	var hashedPassword []byte
+
+	for _, saltedPasswordChar := range saltedPassword {
+		var finalHash byte
+		finalHash = saltedPasswordChar
+		for j := range states {
+			stateChar := states[j]
+			if stateChar == finalHash {
+				val := finalHash*stateChar + 4
+				if val > 127 {
+					finalHash = val - 120
+					states[j] = val ^ stateChar
+				} else if val%2 == 0 {
+					finalHash = val - stateChar
+					states[j] = val + stateChar
+				} else {
+					finalHash = val ^ stateChar
+					states[j] = val + (val ^ (stateChar * 2))
+				}
+			} else if finalHash > stateChar {
+				val := finalHash - stateChar
+				if val > 127 {
+					finalHash = val ^ stateChar
+					states[j] = val - stateChar
+				} else if val%2 == 0 {
+					finalHash = val + stateChar
+					states[j] = (val + stateChar) * 2
+				} else {
+					finalHash = val ^ stateChar
+					states[j] = (val - stateChar) * (val ^ (stateChar * 2))
+				}
+			} else if finalHash < stateChar {
+				val := stateChar - finalHash
+				if val > 127 {
+					finalHash = val ^ stateChar
+					states[j] = val + stateChar
+				} else if val%2 == 0 {
+					finalHash = val - stateChar
+					states[j] = (val - stateChar) * 2
+				} else {
+					finalHash = val ^ stateChar
+					states[j] = (val + stateChar) * (val ^ (stateChar * 2))
+				}
+			}
+		}
+		hashedPassword = append(hashedPassword, finalHash)
+	}
+
+	return hashedPassword, nil
 }
 
-func CheckPasswordHash(password string) bool {
-	return password == HashPassword(password)
+func HashPassword(password string) ([]byte, []byte, error) {
+	salt := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return nil, nil, err
+	}
+
+	hashedPassword, err := HashPasswordWithSalt(password, salt)
+
+	return hashedPassword, salt, err
+}
+
+func CheckPasswordHash(password string, hashed []byte, salt []byte) bool {
+	hashedCheck, err := HashPasswordWithSalt(password, salt)
+	if err != nil {
+		fmt.Println("Error hashing password: ", err)
+		return false
+	}
+	if bytes.Equal(hashed, hashedCheck) {
+		return true
+	}
+
+	return false
 }
