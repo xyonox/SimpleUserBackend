de372604dd73395e3a1b32adb49b4fa5f5be9ac2
xyonox <93388182+xyonox@users.noreply.github.com>
2026-09-01 17:40:19 +0200

    Add: implementing authentication

--- routes/note_routes.go
+++ routes/note_routes.go
@@ -16,6 +16,48 @@ func HttpCreateNote(db *sql.DB) http.HandlerFunc {
 		responseBody := make(map[string]any)
 
 		// TODO implement auth
+		authBool, userID, err := handlers.Authenticate(db, r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			responseBody["error"] = fmt.Sprintf("Authentication failed: %s", err.Error())
+			jsonBody, err := json.Marshal(responseBody)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, err := w.Write([]byte(err.Error()))
+				if err != nil {
+					fmt.Println("Error: ", err)
+					return
+				}
+				fmt.Println("Error: ", err)
+				return
+			}
+			_, err = w.Write(jsonBody)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+			return
+		}
+
+		if !authBool {
+			w.WriteHeader(http.StatusUnauthorized)
+			responseBody["error"] = "Authentication failed"
+			jsonBody, err := json.Marshal(responseBody)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				_, err := w.Write([]byte(err.Error()))
+				if err != nil {
+					fmt.Println("Error: ", err)
+					return
+				}
+				fmt.Println("Error: ", err)
+				return
+			}
+			_, err = w.Write(jsonBody)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+			return
+		}
 
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
@@ -38,7 +80,7 @@ func HttpCreateNote(db *sql.DB) http.HandlerFunc {
 		}
 
 		var simpleNote handlers.SimpleNote
-		err := json.NewDecoder(r.Body).Decode(&simpleNote)
+		err = json.NewDecoder(r.Body).Decode(&simpleNote)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			responseBody["error"] = "Method not allowed"
@@ -58,8 +100,7 @@ func HttpCreateNote(db *sql.DB) http.HandlerFunc {
 			}
 		}
 
-		// TODO implement user id
-		err = handlers.CreateNote(db, simpleNote.Title, simpleNote.Content, 0)
+		err = handlers.CreateNote(db, simpleNote.Title, simpleNote.Content, userID)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			responseBody["error"] = "Method not allowed"
