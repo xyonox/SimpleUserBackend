b33f2dc259c1d1f55d720fdd2137f1c30db5d80e
xyonox <93388182+xyonox@users.noreply.github.com>
2026-09-01 17:18:24 +0200

    Implement logic in the function to manage notes

--- handlers/note_handler.go
+++ handlers/note_handler.go
@@ -12,17 +12,59 @@ type Note struct {
 }
 
 func GetNotesByUserID(db *sql.DB, userID int) ([]*Note, error) {
-	return nil, nil
+	rows, err := db.Query("SELECT * FROM notes WHERE user_id = ?", userID)
+	if err != nil {
+		return nil, err
+	}
+	err = rows.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	var notes []*Note
+	for rows.Next() {
+		note := Note{}
+		err := rows.Scan(&note.ID, &note.Title, &note.Content, &note.CreatedAt, &note.UpdatedAt, &note.UserID)
+		if err != nil {
+			return nil, err
+		}
+		notes = append(notes, &note)
+	}
+
+	return notes, nil
 }
 
-func CreateNote(db *sql.DB, note *Note) error {
+func getNoteByID(db *sql.DB, id int) (*Note, error) {
+	row := db.QueryRow("SELECT * FROM notes WHERE id = ?", id)
+	note := Note{}
+	err := row.Scan(&note.ID, &note.Title, &note.Content, &note.CreatedAt, &note.UpdatedAt, &note.UserID)
+	if err != nil {
+		return nil, err
+	}
+	return &note, nil
+}
+
+func CreateNote(db *sql.DB, title string, content string, userID int) error {
+	note := Note{Title: title, Content: content, UserID: userID}
+	_, err := db.Exec("INSERT INTO notes (title, content, user_id) VALUES (?, ?, ?)", note.Title, note.Content, note.UserID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func UpdateNote(db *sql.DB, note *Note) error {
+	_, err := db.Exec("UPDATE notes SET title = ?, content = ? WHERE id = ?", note.Title, note.Content, note.ID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
-func DeleteNote(db *sql.DB, id int) error {
+func DeleteNote(db *sql.DB, noteID int) error {
+	_, err := db.Exec("DELETE FROM notes WHERE id = ?", noteID)
+	if err != nil {
+		return err
+	}
 	return nil
 }
